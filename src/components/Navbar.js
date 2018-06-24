f87dab760f99df5d4c4034a2d43256df367c0de4
Annia J. Flores <itsandromeda@gmail.com>
2018-06-24 09:21:21 -0500

    Updating dumb & smart components, getting sum of items and sum of total

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My Shop</h1>
-                <span>Total items: <strong>{}</strong></span>
-            </div>
-        ); 
-    }
+const Navbar = (props) => {
+    const items = props.cart.reduce((accumulator, item) => accumulator + (item.quantity),0);
+    
+    return (
+        <div>
+            <h1>My Shop</h1>
+            <span>Total items: <strong>{items}</strong></span>
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
